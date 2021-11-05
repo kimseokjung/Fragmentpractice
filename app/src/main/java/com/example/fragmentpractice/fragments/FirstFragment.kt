17e35dab0fa46d193495a87a63d869a6361fa2fe
Dumcit <slsms311@gmail.com>
2021-11-05 15:30:57 +0900

    first fragment

--- app/src/main/java/com/example/fragmentpractice/fragments/FirstFragment.kt
+++ app/src/main/java/com/example/fragmentpractice/fragments/FirstFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.fragmentpractice.R
+import kotlinx.android.synthetic.main.fragment_first.*
 
 class FirstFragment: Fragment() {
     override fun onCreateView(
@@ -14,5 +15,13 @@ class FirstFragment: Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_first,container,false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        
+        btnChangeText.setOnClickListener { 
+            tvContent.text = "안녕하세요"
+        }
     }
 }
