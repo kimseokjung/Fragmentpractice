29d4826e72c2c7c948e8bc8da06150cdeb2650a1
Dumcit <slsms311@gmail.com>
2021-11-05 19:00:11 +0900

    11

--- app/src/main/java/com/example/fragmentpractice/MainActivity.kt
+++ app/src/main/java/com/example/fragmentpractice/MainActivity.kt
@@ -2,11 +2,17 @@ package com.example.fragmentpractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.fragmentpractice.fragments.SecondFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val f : SecondFragment  = SecondFragment()
+
+        f.isVisible
 
     }
 }
--- app/src/main/java/com/example/fragmentpractice/fragments/SecondFragment.kt
+++ app/src/main/java/com/example/fragmentpractice/fragments/SecondFragment.kt
@@ -1,48 +1,49 @@
 package com.example.fragmentpractice.fragments
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.graphics.drawable.DrawableCompat.inflate
-import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentResultListener
-import androidx.fragment.app.FragmentTransaction
+import androidx.fragment.app.FragmentResultOwner
 import com.example.fragmentpractice.R
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.fragment_first.*
 import kotlinx.android.synthetic.main.fragment_two.*
-import java.util.zip.Inflater
+import kotlinx.android.synthetic.main.fragment_two.*
 
-class FirstFragment : Fragment()  {
+class SecondFragment : Fragment(),FragmentResultListener {
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_first, container, false)
+        return inflater.inflate(R.layout.fragment_two, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-
-        btnChangeText.setOnClickListener {
-            tvContent.text = "안녕하세요"
-        }
-        btnSecondFrag.setOnClickListener {
-            val f: SecondFragment = SecondFragment()
-            val bundle = bundleOf("secondD" to tvContent.text.toString())
-
-            f.arguments = bundle
-
-            activity?.supportFragmentManager!!.beginTransaction()
-                .replace(R.id.secondFrag, SecondFragment()).commit()
+        FragmentResultListener { requestKey, result ->
+            val bundle = result.getString("secondD")
+            if (bundle != null) {
+                tvContentSecond.text = bundle
+                Log.d("TAG", "$bundle")
+            }
 
         }
+//        val bundle = arguments?.getString("secondD")
+//        if (bundle != null) {
+//            tvContentSecond.text = bundle
+//            Log.d("TAG", "$bundle")
+//        }
+
     }
 
-
+    override fun onFragmentResult(requestKey: String, result: Bundle) {
+        TODO("Not yet implemented")
+    }
 }
